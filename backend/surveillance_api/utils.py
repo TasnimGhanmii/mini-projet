edacd884e12d55eec6521cdfcebbcbe25b1f45b7
AyettGh <adelejennifer5@gmail.com>
2025-04-03 12:03:12 +0200

    update

--- backend/surveillance_api/utils.py
+++ backend/surveillance_api/utils.py
@@ -1,8 +1,11 @@
+from surveillance_api.models import Formula
+
+
 def calculate_max_hours(row):
     formula_instance = Formula.objects.first()
-    formula = formula_instance.formula if formula_instance else "courses + td + tp"
+    formula = formula_instance.formula if formula_instance else "(courses + td + tp * 0.75) * coef"
     try:
-        max_hours = eval(formula, {"courses": row["Cours"], "td": row["TD"], "tp": row["TP"]})
+        max_hours = round(eval(formula, {"courses": row["Cours"], "td": row["TD"], "tp": row["TP"]}))
         return max_hours
     except Exception as e:
         print(f"Error evaluating formula: {e}")
